a01df9811e2267cc35b9db796f5d86d6b84353a9
Ariel <ariel@ArielnoMacBook-Air.local>
2024-10-25 17:55:04 +0900

    fixed

--- app.py
+++ app.py
@@ -5,14 +5,23 @@ app = Flask(__name__)
 
 CORS(app)
 
-data = {
-    "message": "Hello , Py!"
-}
+
+@app.route('/', defaults={'path':''}, methods=['GET'])
+@app.route('/<path:path>', methods=['GET'])
+def hello(path):
+    return jsonify(
+        {
+            'message': 'Hello, this is an anonymous route!',
+            'path': path,
+            'dir': 'root/main'
+        }
+    )
 
 
-@app.route('/api/v1/hello', methods=['GET'])
-def hello():
-    return jsonify(data)
+with app.app_context():
+    for rule in app.url_map.iter_rules():
+        print(f"Endpoint: {rule.endpoint}, Route: {rule.rule}")
+
 
 if __name__ == '__main__':
     app.run(debug=True)
